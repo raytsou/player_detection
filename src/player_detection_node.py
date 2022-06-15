67e151504700c3d8efea5d7c9232249fe56490b9
Ray Tsou <raytsou1223@gmail.com>
2022-06-15 12:55:50 -0400

    Connected Node to Sequoia Detector
    Detection node subscribes to Image msg and publishes BoundingBox2DArray. Upon
    receiving an image, we immediately call the detector class, which loads weights
    from Sequoia, detects, and passes out the bounding boxes. We also have option
    to visualize, which will publish another Image with the bounding boxes drawn.
    
    TODO: Make node publish at fixed rate

--- src/player_detection_node.py
+++ src/player_detection_node.py
@@ -3,24 +3,63 @@
 import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
+from vision_msgs.msg import BoundingBox2D, BoundingBox2DArray
+from geometry_msgs.msg import Pose2D
+from cv_bridge import CvBridge
+from player_detection.detector import SequoiaDetector
 
 class PlayerDetectionNode(object):
   def __init__(self):
     """Initializes the node"""
-    self.node_name = rospy.get_name()
-
     # Get parameters
     self.rate = rospy.get_param("~rate", 10)
     self.screen_msg = rospy.get_param("~screen_msg")
+    self.visualize = rospy.get_param("~viz", default=False)
+    wdir = rospy.get_param("~weights_dir", default=None)
+
+    # Setup
+    self.node_name = rospy.get_name()
+    self.bridge = CvBridge()
+    self.detector = SequoiaDetector(weights_dir=wdir)
+    self.latest_img = None
+    self.labels = ['t', 'ct']
 
     # Setup the publisher and subscriber
     self.sub = rospy.Subscriber(self.screen_msg, Image, self.callback)
-    self.pub = rospy.Publisher('~test', String, queue_size=1)
+    self.pubs = {label: rospy.Publisher('~' + label, BoundingBox2DArray, queue_size=1) for label in self.labels}
+    self.pub_viz = rospy.Publisher('~bbox_visualized', Image, queue_size=1)
+
 
   def callback(self, msg):
-    hello = "hello world"
-    rospy.loginfo(hello)
-    self.pub.publish(hello)
+    self.latest_img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+    self.publish()
+
+  def publish(self):
+    detections = self.detector.detect(self.latest_img)
+
+    for label in self.labels:
+      msg = BoundingBox2DArray()
+      pub = self.pubs[label]
+
+      for x1, y1, x2, y2 in detections[label]:
+        ctr = Pose2D()
+        ctr.x = (x2+x1) / 2
+        ctr.y = (y2+y1) / 2
+
+        box = BoundingBox2D()
+        box.center = ctr
+        box.size_x = x2-x1
+        box.size_y = y2-y1
+
+        msg.boxes.append(box)
+
+      pub.publish(msg)
+
+    if self.visualize:
+      img = self.detector.visualize()
+      img_msg = self.bridge.cv2_to_imgmsg(img, encoding="passthrough")
+
+      self.pub_viz.publish(img_msg)
 
   def loginfo(self, msg):
     rospy.loginfo(f"[{self.node_name}] {msg}")
